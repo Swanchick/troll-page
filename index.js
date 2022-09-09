2d5ac30daf60363ffd3bc12f312d9c96acfd90aa
Swanchick <61909965+Swanchick@users.noreply.github.com>
2022-09-09 21:38:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ var urlCity = "http://www.geoplugin.net/json.gp"
 var ipItem = document.getElementById("ip")
 var cityCountryItem = document.getElementById("country-city")
 
-document.addEventListener("DOMContentLoaded", function(){
+function getCityCountry(ip){
+    let url = `${urlCity}?ip=${ip}`
+
+    console.log(url)
+
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        cityCountryItem.innerHTML = `${json.geoplugin_city}, ${json.geoplugin_countryName}`
+    })
+}
+
+function getIP(){
     fetch(urlIP)
     .then(res => res.text())
     .then(text => {
         document.title = text
         ipItem.innerHTML = text
-    
+        getCityCountry(text)
     })
-    
-    fetch(urlCity)
-    .then(res => res.json())
-    .then(json => {
-        cityCountryItem.innerHTML = `${json.geoplugin_city}, ${json.geoplugin_countryName}`
-    })
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    getIP()
 })
